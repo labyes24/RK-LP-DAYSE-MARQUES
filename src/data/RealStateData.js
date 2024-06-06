1307b04ca4a4be5c3b096c0d8de01043c81ae21e
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2024-06-05 21:19:04 -0300

    added spanish translation to properties description

--- src/data/RealStateData.js
+++ src/data/RealStateData.js
@@ -93,9 +93,77 @@ const propertiesPT = [
   },
 ]
 
-/**  @type {Array<Property>} The Portuguese properties data */
+/**  @type {Array<Property>} The Spanish properties data */
 const propertiesES = [
-  // TODO: Add Spanish properties text
+  {
+    id: crypto.randomUUID(),
+    brokerId: BROKER_ID,
+    RKCode: '9972',
+    title: 'Brisamar',
+    region: 'Ingleses',
+    description:
+      '**PROPIEDAD CON REGISTRO**\n\n Hermoso y espacioso departamento, dos habitaciones, 1 suite, balcón con parrilla, completamente amueblado en condominio con piscina, en una de las mejores calles de Praia dos Ingleses. Situado a 500 metros del mar.',
+    price: 'R$ 650.000,00',
+    details: {
+      square: '78,52 m²',
+      rooms: '2',
+      bathrooms: '2',
+      garage: '2',
+    },
+    pictures: [
+      './properties/brisamar-f05.jpg',
+      './properties/brisamar-f01.jpg',
+      './properties/brisamar-f02.jpg',
+      './properties/brisamar-f03.jpg',
+      './properties/brisamar-f04.jpg',
+    ],
+  },
+  {
+    id: crypto.randomUUID(),
+    brokerId: BROKER_ID,
+    RKCode: '7581',
+    title: 'Ilha das Galés',
+    region: 'Ingleses',
+    description:
+      '**PROPIEDAD CON REGISTRO**\n\n Maravilloso penthouse de 4 recámaras, 3 suites, amplia terraza con alberca, sala de dos ambientes, completamente amueblado, con vista al mar, ubicado en la zona privilegiada del barrio y a una cuadra de la playa.',
+    price: 'R$ 2.600.000,00',
+    details: {
+      square: '235,47 m²',
+      rooms: '4',
+      bathrooms: '5',
+      garage: '2',
+    },
+    pictures: [
+      './properties/ilha-f05.jpg',
+      './properties/ilha-f01.jpg',
+      './properties/ilha-f03.jpg',
+      './properties/ilha-f04.jpg',
+      './properties/ilha-f02.jpg',
+    ],
+  },
+  {
+    id: crypto.randomUUID(),
+    brokerId: BROKER_ID,
+    RKCode: '9839',
+    title: 'Ponta do Mar',
+    region: 'Ingleses',
+    description:
+      '**PROPIEDAD CON REGISTRO**\n\n Si busca un apartamento espacioso cerca del mar, esta es la propiedad adecuada.\n\n Apartamento amueblado, de 3 habitaciones, una de ellas suite con balcón, cocina amueblada e integrada al salón, amplio balcón con barbacoa de carbón, plaza de aparcamiento y hobby box.\n\n El condominio ofrece piscina, salón de fiestas, ascensor y seguridad.',
+    price: 'R$ 739.000,00',
+    details: {
+      square: '105,30 m²',
+      rooms: '3',
+      bathrooms: '2',
+      garage: '1',
+    },
+    pictures: [
+      './properties/ponta-f01.jpg',
+      './properties/ponta-f02.jpg',
+      './properties/ponta-f04.jpg',
+      './properties/ponta-f03.jpg',
+      './properties/ponta-f05.jpg',
+    ],
+  },
 ]
 
 /**
@@ -108,7 +176,7 @@ const propertiesES = [
 /** @type {PropertiesData} The properties data */
 const properties = {
   pt: propertiesPT,
-  // es: propertiesES,
+  es: propertiesES,
 }
 
 /**
